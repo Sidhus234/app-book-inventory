b6164ade4fa93e539c5c7df17d1cd9ab8e3f84b2
Gursewak Singh Sidhu <sidhus234@gmail.com>
2022-05-04 08:15:23 +0800

    Create book_store_backend.py

---
+++ book_store_backend.py
@@ -0,0 +1,71 @@
+import sqlite3
+
+
+def connect():
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute(
+        "CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title TEXT, author TEXT, year INTEGER, isbn INTEGER)")
+    conn.commit()
+    conn.close()
+    pass
+
+
+# Everytime we run gui (and import backend) this will ensure the db is up and running
+connect()
+
+
+def insert(title, author, year, isbn):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("INSERT INTO book VALUES (NULL, ? ,?, ?, ?)",
+                (title, author, year, isbn))
+    conn.commit()
+    conn.close()
+    pass
+
+
+def view():
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM book")
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+
+def search(title="", author="", year="", isbn=""):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=?",
+                (title, author, year, isbn))
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+
+def delete(id):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("DELETE FROM book WHERE id=?", (id, ))
+    conn.commit()
+    conn.close()
+    pass
+
+
+def update(id, title, author, year, isbn):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("UPDATE book SET title=?, author=?, year=?, isbn=? WHERE id=?", (title, author, year, isbn, id))
+    conn.commit()
+    conn.close()
+    pass
+
+insert("The Seat", "John Tablet", 1918, 232472962)
+print(view())
+insert("Apple", "John Smith", 1987, 354873)
+print(search(author="John Smith"))
+delete(3)
+print(view())
+update(1, "The Moon", "John Tablet", 1918, 232472962)
+print(view())
